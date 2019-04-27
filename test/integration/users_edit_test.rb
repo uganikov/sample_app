9f202e86a0eb083723badeb869484fa7953fe017
Shingo Akagaki <a1901sa@aiit.ac.jp>
2019-04-28 00:11:26 +0900

    10.2

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -34,4 +34,23 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_equal name,  @user.name
     assert_equal email, @user.email
   end
+
+  test "successful edit with friendly forwarding" do
+    get edit_user_path(@user)
+    assert_equal session[:forwarding_url], edit_user_url(@user)
+    log_in_as(@user)
+    assert_redirected_to edit_user_url(@user)
+    assert_not session[:forwarding_url]
+    name  = "Foo Bar"
+    email = "foo@bar.com"
+    patch user_path(@user), params: { user: { name:  name,
+                                              email: email,
+                                              password:              "",
+                                              password_confirmation: "" } }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name,  @user.name
+    assert_equal email, @user.email
+  end
 end
