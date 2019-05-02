ac3d8dbabf2e4a5bfedf36d67a4106175622188f
Shingo Akagaki <a1901sa@aiit.ac.jp>
2019-05-03 02:09:18 +0900

    suppress the waste query

--- app/models/user.rb
+++ app/models/user.rb
@@ -80,7 +80,7 @@ class User < ApplicationRecord
     following_ids = "SELECT followed_id FROM relationships
                      WHERE follower_id = :user_id"
     Micropost.where("user_id IN (#{following_ids})
-                     OR user_id = :user_id", user_id: id)
+                     OR user_id = :user_id", user_id: id).includes(:user)
   end
 
   # ユーザーをフォローする
