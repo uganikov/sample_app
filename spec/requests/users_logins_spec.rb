fe99144c026a844c7e2630cd54e6c4c4d0e8bfdc
Shingo Akagaki <a1901sa@aiit.ac.jp>
2019-05-02 22:54:13 +0900

    use capybara session

--- spec/requests/users_logins_spec.rb
+++ spec/requests/users_logins_spec.rb
@@ -12,25 +12,40 @@ RSpec.describe "UsersLogins", type: :request do
   end
 
   it "login with valid information followed by logout" do
-    visit login_path
-    fill_in "Email", with: user.email
-    fill_in "Password", with: 'password'
-    click_on "commit"
-    expect(is_logged_in?).to be_truthy
-    expect(page.current_url).to eq user_url(user)
-    expect(page).not_to have_selector "a[href='#{login_path}']"
-    expect(page).to have_selector "a[href='#{logout_path}']" 
-    expect(page).to have_selector "a[href='#{user_path(user)}']"
-    click_on "Log out"
-    expect(is_logged_in?).to be_falsey
-    expect(page.current_url).to eq root_url
+    using_session :session1 do
+      visit login_path
+      expect(is_logged_in?).to be_falsey
+      fill_in "Email", with: user.email
+      fill_in "Password", with: 'password'
+      click_on "commit"
+      expect(is_logged_in?).to be_truthy
+      expect(page.current_url).to eq user_url(user)
+      expect(page).not_to have_selector "a[href='#{login_path}']"
+      expect(page).to have_selector "a[href='#{logout_path}']" 
+      expect(page).to have_selector "a[href='#{user_path(user)}']"
+    end
 
     # 2番目のウィンドウでログアウトをクリックするユーザーをシミュレートする
-    delete logout_path
-    follow_redirect!
-    assert_select "a[href=?]", login_path
-    assert_select "a[href=?]", logout_path,      count: 0
-    assert_select "a[href=?]", user_path(user), count: 0
+    using_session :session2 do
+      visit login_path
+      expect(is_logged_in?).to be_falsey
+      fill_in "Email", with: user.email
+      fill_in "Password", with: 'password'
+      click_on "commit"
+      expect(is_logged_in?).to be_truthy
+    end
+
+    using_session :session1 do
+      click_on "Log out"
+      expect(is_logged_in?).to be_falsey
+      expect(page.current_url).to eq root_url
+    end
+
+    using_session :session2 do
+      click_on "Log out"
+      expect(is_logged_in?).to be_falsey
+      expect(page.current_url).to eq root_url
+    end
   end
 
   it "login with remembering" do
