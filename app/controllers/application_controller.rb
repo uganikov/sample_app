9fbf66e9d5c86c5507d919b4b66fcaf38c501f5a
Shingo Akagaki <a1901sa@aiit.ac.jp>
2019-05-04 02:27:17 +0900

    initialize I18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
   protect_from_forgery with: :exception
   include SessionsHelper
 
@@ -11,6 +12,24 @@ protected
       store_location
       flash[:danger] = "Please log in."
       redirect_to login_url
+    end
+  end
+
+private
+  def set_locale
+    begin
+      I18n.locale = params[:locale] || I18n.default_locale
+      # RSpec はこれがないとデフォ値がつかないようだ。
+      Rails.application.routes.default_url_options =  { locale: params[:locale] }
+      # local サーバはこっちがないとデフォ値がつかないようだ。
+      self.default_url_options =  { locale: params[:locale] }
+      # 挙動がちがうのでこれで試験になっているとはいいがたい。
+      # そして、ActionMailer にはこれがいる。
+      ActionMailer::Base.default_url_options.merge!({ locale: params[:locale] })
+    rescue I18n::InvalidLocale
+      self.default_url_options[:locale] = nil
+      Rails.application.routes.default_url_options[:locale] = nil
+      ActionMailer::Base.default_url_options[:locale] = nil
     end
   end
 end
