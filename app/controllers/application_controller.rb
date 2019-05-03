d17bd119e9940c28aeec29c27f36fb52033ce7de
Shingo Akagaki <a1901sa@aiit.ac.jp>
2019-05-04 03:40:32 +0900

    lang negotiation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,8 +17,9 @@ protected
 
 private
   def set_locale
+    locale = request.env['HTTP_ACCEPT_LANGUAGE'] ? request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first : nil
     begin
-      I18n.locale = params[:locale] || I18n.default_locale
+      I18n.locale = params[:locale] || locale || I18n.default_locale
       # RSpec はこれがないとデフォ値がつかないようだ。
       Rails.application.routes.default_url_options =  { locale: params[:locale] }
       # local サーバはこっちがないとデフォ値がつかないようだ。
@@ -27,6 +28,7 @@ private
       # そして、ActionMailer にはこれがいる。
       ActionMailer::Base.default_url_options.merge!({ locale: params[:locale] })
     rescue I18n::InvalidLocale
+      I18n.locale = I18n.default_locale
       self.default_url_options[:locale] = nil
       Rails.application.routes.default_url_options[:locale] = nil
       ActionMailer::Base.default_url_options[:locale] = nil
--- spec/requests/i18n_spec.rb
+++ spec/requests/i18n_spec.rb
@@ -1,19 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe "I18n", type: :request do
-  it "works" do
-    url = root_url
-    expect(url).not_to  match %r{/(ja|en)/}
+  let(:url) { root_url }
+  let(:user) { users(:michael) }
+
+  it "is English in default" do
+    log_in_as(user)
+    expect(url).not_to match %r{/(ja|en)/}
     visit url
     expect(page.current_url).to eq url
+    expect(page).not_to have_content "分"
+  end
 
+  it "is Japanese when instruct by query parameter or url" do
+    log_in_as(user)
     visit "#{url}?locale=ja"
     click_on("Home")
     expect(page.current_url).to eq root_url(locale: :ja)
-    expect(page.current_url).to match %r{/ja}
+    expect(page).to have_content "分"
+  end
 
+  it "is default behaovr when instruct wrong locale" do
+    log_in_as(user)
     visit "#{url}?locale=hoge"
     click_on("Home")
     expect(page.current_url).to eq url
+    expect(page).not_to have_content "分"
   end
+
+  it "is Japanese when instruct by Accept-Language" do
+    log_in_as(user)
+    page.driver.header('Accept-Language', "ja-JP")
+    visit "#{url}"
+    click_on("Home")
+    expect(page.current_url).to eq root_url
+    expect(page).to have_content "分"
+  end
+
 end
